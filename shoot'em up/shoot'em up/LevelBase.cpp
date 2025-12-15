32ec7a4df20bb4a47a0ce77133b3e8989a8642e2
lozacobac <lozacobac@gmail.com>
2025-12-15 10:31:12 +0100

    dragonHealth resolved

--- shoot'em up/shoot'em up/LevelBase.cpp
+++ shoot'em up/shoot'em up/LevelBase.cpp
@@ -1,16 +1,16 @@
 #include "LevelBase.h"
 #include <iostream>
-#include "Enemy.h"
+#include "Projectile.h"
 
 
 
 LevelBase::LevelBase(TTF_Font* font)
-    : currentCommand(0),
+    : font(font),
+    currentCommand(0),
     levelStartTime(0),
     levelCompleted(false),
     levelFailed(false),
     score(0),
-    font(font),
     DragonHealth(100)
 {
     menuButton = createButton(10.0f, 425.0f, 75.0f, 50.0f, "Menu");
@@ -27,6 +27,7 @@ bool LevelBase::loadFromFile(const std::string& scriptPath) {
     levelCompleted = false;
     levelFailed = false;
     score = 0;
+    DragonHealth = 100;
     enemies.clear();
     allProjectiles.clear();
 
@@ -66,33 +67,30 @@ void LevelBase::handleCollisions() {
         bool hit = false;
 
         if (pit->isPlayer) {
-
             for (auto eit = enemies.begin(); eit != enemies.end(); ) {
                 if ((*eit)->checkCollision(pit->rect)) {
-                    score += 100;
-                    std::cout << "[Info] Enemy destroyed! Score = " << score << "\n";
-                    eit = enemies.erase(eit);
-                    hit = true;
-                    break;
                     if ((*eit)->getType() == 9) {
                         DragonHealth--;
-                        std::cout << "Dragon a �t� toucher" << DragonHealth;
+                        std::cout << "Dragon touche HP restant: " << DragonHealth << "\n";
                         hit = true;
+                        
                         if (DragonHealth <= 0) {
                             levelCompleted = true;
-                            std::cout << "dragon killed";
+                            score += 500;
+                            std::cout << "Dragon vaincu\n";
                             eit = enemies.erase(eit);
-                            break;
                         }
-                        break;
+                        else {
+                            ++eit;
+                        }
                     }
                     else {
+                        score += 100;
+                        std::cout << "Enemy destroyed Score = " << score << "\n";
                         eit = enemies.erase(eit);
                         hit = true;
-                        std::cout << "[INFO] Enemy destroyed!\n";
-                        break;
                     }
-                    
+                    break;
                 }
                 else {
                     ++eit;
@@ -104,7 +102,7 @@ void LevelBase::handleCollisions() {
                 if (!player.isInvincible()) {
                     player.lives--;
                     player.invicibilityTimer = 1.0f;
-                    std::cout << "[INFO] Player hit! Lives : " << player.lives << "\n";
+                    std::cout << "Player hit! Lives : " << player.lives << "\n";
                     if (player.lives <= 0) {
                         levelFailed = true;
                     }
@@ -127,7 +125,6 @@ void LevelBase::handleCollisions() {
             player.projectiles.push_back(p);
         }
     }
-
 }
 
 void LevelBase::handleEvent(const SDL_Event& event, bool& shouldSwitchToMenu) {
